fe7fe6b1761c013e2f0169f2aa139aa0a10d41a4
Perarivalan <arivu1820@gmail.com>
2024-11-24 20:10:19 +0530

    Data reviews added token and tagged

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import kagglehub
 
 
 #read in data
-df = pd.read_csv('D:\Perarivalan\Sentiment-Analysis-Project\TestReviews.csv')
+df = pd.read_csv('D:\Perarivalan\TestReviews.csv')
 print(df.shape)
 df = df.head(500)
 print(df.shape)
@@ -20,6 +20,18 @@ df.head()
 
 
 
-countgraph = df['review'].value_counts().sort_index().plot(kind='bar',title='Count of review',figsize=(10,5))
+countgraph = df['class'].value_counts().sort_index().plot(kind='bar',title='Count of review',figsize=(10,5))
 countgraph.set_xlabel('count')
 plt.show()
+
+example = df['review'][50]
+print(example)
+
+tokens = nltk.word_tokenize(example)
+tokens[:10]
+
+tagged = nltk.pos_tag(tokens)
+tagged[:10]
+
+entities = nltk.chunk.ne_chunk(tagged)
+entities.pprint()
