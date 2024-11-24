4ab892939b174b6cc8ebc52b6fd165e340f7dfb6
Perarivalan <arivu1820@gmail.com>
2024-11-24 21:17:35 +0530

    Added sample example and placed title for the reviews

---
+++ Senimentsscoring.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import main
+
+# Seniment Scoring
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
+
+sa = SentimentIntensityAnalyzer()
+
+res = {}
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    text = row['review']
+    res = sa.polarity_scores(text)
+
+
+vaders = pd.DataFrame(res).T
+vaders = vaders.reset_index().rename(columns={'index': 'Id'})
+vaders = vaders.merge(df, how='left')
+
+ax = sns.barplot(data=vaders, x='Score', y='compound')
+ax.set_title('Compund Score by Amazon Star Review')
+plt.show()
