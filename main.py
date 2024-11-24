5f126c9fe63b7437fb6dacc29a94abb3f7b5b833
Perarivalan <arivu1820@gmail.com>
2024-11-24 21:16:17 +0530

    Added sample example and placed title for the reviews

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ df.head()
 
 countgraph = df['class'].value_counts().sort_index().plot(kind='bar',title='Count of review',figsize=(10,5))
 countgraph.set_xlabel('count')
-plt.show()
+# plt.show()
 
 example = df['review'][50]
 print(example)
@@ -35,3 +35,34 @@ tagged[:10]
 
 entities = nltk.chunk.ne_chunk(tagged)
 entities.pprint()
+
+# Seniment Scoring
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
+
+sa = SentimentIntensityAnalyzer()
+
+res = {}
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    text = row['review']
+    res = sa.polarity_scores(text)
+
+
+vaders = pd.DataFrame(res).T
+vaders = vaders.reset_index().rename(columns={'index': 'Id'})
+vaders = vaders.merge(df, how='left')
+
+ax = sns.barplot(data=vaders, x='Score', y='compound')
+ax.set_title('Compund Score by Amazon Star Review')
+plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 3))
+sns.barplot(data=vaders, x='Score', y='pos', ax=axs[0])
+sns.barplot(data=vaders, x='Score', y='neu', ax=axs[1])
+sns.barplot(data=vaders, x='Score', y='neg', ax=axs[2])
+axs[0].set_title('Positive')
+axs[1].set_title('Neutral')
+axs[2].set_title('Negative')
+plt.tight_layout()
+plt.show()
